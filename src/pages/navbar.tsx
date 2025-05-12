0bcb89958461ed0a5cde249570454d242322080d
vdanielvazquez <v.danielvazquez.m@gmail.com>
2025-05-11 21:40:56 -0700

    titulo del proyecto

--- src/pages/navbar.tsx
+++ src/pages/navbar.tsx
@@ -6,7 +6,7 @@ const Navbar: React.FC = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top">
       <div className="container-fluid">
-        <Link className="navbar-brand" to="/">Sistema Web Pro</Link>
+        <Link className="navbar-brand" to="/">Sistema Web Pro V2</Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
