35ef512c199cbbd817f078375adbae7a575b7967
vdanielvazquez <v.danielvazquez.m@gmail.com>
2025-05-23 10:55:03 -0700

    mapa de ubicacion

--- src/pages/NuevoNegocio.tsx
+++ src/pages/NuevoNegocio.tsx
@@ -403,12 +403,13 @@ const geocodeLocation = async (estado: string, municipio: string) => {
                 <MapContainer
   center={mapCenter}
   zoom={13}
-  style={{ height: "600px", width: "100%" }}
+  style={{ height: "600px", width: "600px" }}
 >
   <TileLayer
-    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-  />
+  attribution='Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+  url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
+/>
+
   <MapCenterUpdater center={mapCenter} />
   <MapClickHandler setFormData={setFormData} />
   {formData.latitud !== undefined && formData.longitud !== undefined && (
