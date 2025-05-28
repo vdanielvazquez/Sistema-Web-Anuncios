8bdefa13abd5a9828f8875c378c20d67a2cbb1e6
vdanielvazquez <v.danielvazquez.m@gmail.com>
2025-05-28 12:26:42 -0700

    datos clientes

--- src/pages/DatosCliente.tsx
+++ src/pages/DatosCliente.tsx
@@ -54,48 +54,50 @@ const DatosCliente = () => {
 
   // Mostrar la portada de los negocios
   return (
-    <div className="container mt-5">
-      <h2 className="text-center mb-4">Datos del Cliente</h2>
-      <div className="row">
-        <div className="col-md-4">
-          <div className="card">
-            <div className="card-body">
-              <h5 className="card-title">{cliente.nombre}</h5>
-              <p className="card-text"><strong>Teléfono: </strong> {cliente.telefono}</p>
-              <p className="card-text"><strong>Email: </strong> {cliente.correo}</p>
-              <a className="btn btn-primary">Editar</a>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <h3 className="text-center mt-5">Negocios de {cliente.nombre}</h3>
-      <div className="row">
-        {negocios.length > 0 ? (
-          negocios.map((negocio) => (
-            <div key={negocio.idnegocio} className="col-md-4 mb-4">
-              <div className="card">
-                <img
-                  src={
-                    negocio.portada
-                      ? `${API_URL}/${negocio.idnegocio}/${negocio.portada}`
-                      : 'default-image.jpg'  // Si no tiene portada, muestra una imagen por defecto
-                  }
-                  className="card-img-top"
-                  alt="Negocio"
-                />
+    <div className="divprincipal">
+        <div className="container">
+        <h2 className="text-center mt-5">Datos del Cliente</h2>
+        <div className="row">
+            <div className="col-md-4">
+            <div className="card">
                 <div className="card-body">
-                  <p>Nombre Comercial<h3>{negocio.nombre_comercial}</h3></p>
-                  <p>Estatus<h3>{negocio.telefono}</h3></p>
-                  <a href={`/negocio/${negocio.idnegocio}`} className="btn btn-primary">Ver más</a>
+                <h5 className="card-title">{cliente.nombre}</h5>
+                <p className="card-text"><strong>Teléfono: </strong> {cliente.telefono}</p>
+                <p className="card-text"><strong>Email: </strong> {cliente.correo}</p>
+                <a className="btn btn-primary">Editar</a>
                 </div>
-              </div>
             </div>
-          ))
-        ) : (
-          <p className="text-center">Este cliente aún no tiene negocios registrados.</p>
-        )}
-      </div>
+            </div>
+        </div>
+
+        <h3 className="text-center mt-5">Negocios de {cliente.nombre}</h3>
+        <div className="row">
+            {negocios.length > 0 ? (
+            negocios.map((negocio) => (
+                <div key={negocio.idnegocio} className="col-md-4 mb-4">
+                <div className="card">
+                    <img
+                    src={
+                        negocio.portada
+                        ? `${API_URL}/${negocio.idnegocio}/${negocio.portada}`
+                        : 'default-image.jpg'  // Si no tiene portada, muestra una imagen por defecto
+                    }
+                    className="card-img-top"
+                    alt="Negocio"
+                    />
+                    <div className="card-body">
+                    <p>Nombre Comercial<h3>{negocio.nombre_comercial}</h3></p>
+                    <p>Estatus<h3>{negocio.telefono}</h3></p>
+                    <a href={`/negocio/${negocio.idnegocio}`} className="btn btn-primary">Ver más</a>
+                    </div>
+                </div>
+                </div>
+            ))
+            ) : (
+            <p className="text-center">Este cliente aún no tiene negocios registrados.</p>
+            )}
+        </div>
+        </div>
     </div>
   );
 };
