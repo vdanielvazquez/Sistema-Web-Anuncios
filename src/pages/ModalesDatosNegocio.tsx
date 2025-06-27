803d9505a222c49f7226bf00979c11a13d98bc25
vdanielvazquez <v.danielvazquez.m@gmail.com>
2025-06-27 08:36:37 -0700

    promocion

--- src/pages/ModalesDatosNegocio.tsx
+++ src/pages/ModalesDatosNegocio.tsx
@@ -168,7 +168,7 @@ export const ModalEditarInfoNegocio: React.FC<ModalEditarInfoNegocioProps> = ({
           <input
             type="text"
             className="form-control"
-            name="promocion"
+            name="concicion"
             value={editForm.condicion || ''}
             onChange={handleInputChange}
           />
