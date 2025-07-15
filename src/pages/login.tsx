99493b987a3d867a17240fd6e003b845cf00b847
vdanielvazquez <v.danielvazquez.m@gmail.com>
2025-07-14 22:03:00 -0700

    diseño de login

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -2,6 +2,7 @@ import React, { useState, useContext } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import { AuthContext } from '../context/AuthContext';
+import '../css/login.css';
 
 const Login = () => {
   const [nombre, setNombre] = useState('');
@@ -27,40 +28,17 @@ const Login = () => {
   };
 
   return (
-    <div className="divprincipal">
-       <div className="d-flex justify-content-center align-items-center vh-100 bg-light">
-      <div className="card shadow p-4" style={{ width: '100%', maxWidth: '400px' }}>
-        <h3 className="text-center mb-4">Iniciar Sesión</h3>
-        <form onSubmit={handleLogin}>
-          <div className="mb-3">
-            <label className="form-label">Usuario</label>
-            <input
-              type="text"
-              className="form-control"
-              value={nombre}
-              onChange={(e) => setNombre(e.target.value)}
-              required
-              autoFocus
-            />
-          </div>
-          <div className="mb-3">
-            <label className="form-label">Contraseña</label>
-            <input
-              type="password"
-              className="form-control"
-              value={contrasena}
-              onChange={(e) => setContrasena(e.target.value)}
-              required
-            />
-          </div>
-          <button type="submit" className="btn btn-primary w-100">
-            Ingresar
-          </button>
-        </form>
-      </div>
+    <div className="page-login">
+      <div className="container">
+          <div className="heading">Iniciar Sesión</div>
+          <form onSubmit={handleLogin}  className="form">
+            <input  type="text"  className="input" value={nombre} onChange={(e) => setNombre(e.target.value)} required  autoFocus/>
+            <input type="password" className="input" value={contrasena} onChange={(e) => setContrasena(e.target.value)} required/>
+            <button type="submit" className="btn login-button">Ingresar</button>
+          </form>
+        <span className="agreement"><a href="#">Learn user licence agreement</a></span>
+      </div> 
     </div>
-    </div>
-   
   );
 };
 
